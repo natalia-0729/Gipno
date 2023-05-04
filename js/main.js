52ee6873d6c040821c339c1a5aa6a2f7400d61ba
Natalia Cheban <114994552+natalia-0729@users.noreply.github.com>
2023-05-04 16:36:05 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,6 +18,23 @@ $(function(){
     $(this).toggleClass('open');
   });
 
+  //E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
   
   $('.comments__inner').slick({
     dots: true,
